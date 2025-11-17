d1360fee2cdf4edae51e61d7b2d96f458e48e7a0
LinusTC <tongchun03@gmail.com>
2025-11-17 19:28:04 +0800

    init

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+w_dict = {f'w{i}' for i in range(1, 11)}
+t_dict = {f't{i}' for i in range(1, 11)}
+s_dict = {f's{i}' for i in range(1, 11)}
+f_dict = {'east', 'south', 'west', 'north', 'mid', 'fa', 'door'}
---
+++ main.py
@@ -0,0 +1,11 @@
+from dictionary import f_dict, w_dict, t_dict, s_dict
+
+def count_compass(winner_seat):
+    count = 0
+
+    
+
+    return count
+
+if __name__ == "__main__":
+    print(w_dict, t_dict, s_dict, f_dict)
