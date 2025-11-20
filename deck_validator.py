f4dca139200caebc02b4dfe3fba521924ea5be97
LinusTC <tongchun03@gmail.com>
2025-11-20 18:36:04 +0800

    validator complete, returns correctly

--- deck_validator.py
+++ deck_validator.py
@@ -12,27 +12,33 @@ class DeckValidator:
             return False
         
         #Check flower hu
-        if self.flower_hu_check(self.winner_tiles):
-            self.possibleDecks.append(['flower hu', self.winner_tiles])
+        flower_results = self.flower_hu_check(self.winner_tiles)
+        if flower_results:
+            self.possibleDecks.append(flower_results)
             return True
         
         #Check Ligu
-        if self.ligu_check(self.winner_tiles_no_flower):
-            self.possibleDecks.append(['ligu', self.winner_tiles])
+        ligu_results = self.ligu_check(self.winner_tiles_no_flower)
+        if ligu_results:
+            self.possibleDecks.append(ligu_results)
             return True
         
         #Check 16bd
-        if self.sixteen_bd_check(self.winner_tiles_no_flower):
-            self.possibleDecks.append(['16bd', self.winner_tiles])
+        sixteen_bd_results = self.sixteen_bd_check(self.winner_tiles_no_flower)
+        if sixteen_bd_results:
+            self.possibleDecks.append(sixteen_bd_results)
             return True
         
         #Check 13 waist
-        if self.thirteen_waist_check(self.winner_tiles_no_flower):
-            self.possibleDecks.append(['13waist', self.winner_tiles])
+        thirteen_results = self.thirteen_waist_check(self.winner_tiles_no_flower)
+        if thirteen_results:
+            self.possibleDecks.append(thirteen_results)
             return True
         
-        #Check Standard, already adds to possible decks on its own
-        if self.standard_check(self.winner_tiles_no_flower):
+        #Check Standard
+        standard_results = self.standard_check(self.winner_tiles_no_flower)
+        if standard_results:
+            self.possibleDecks.append(standard_results)
             return True
 
         return False
@@ -46,48 +52,50 @@ class DeckValidator:
         return count
     
     def flower_hu_check(self, tiles):
-        flower_count = 0
+        flowers = []
 
         for key in tiles:
             if key in flower_dict:
-                flower_count += 1
+                flowers.append(key)
 
-        if flower_count > 6:
-            return True
-        
-        return False
+        if len(flowers) >= 7:
+            return ['flower hu', {'flowers': flowers}]
+
+        return []
     
     def ligu_check(self, tiles):
-        
-        pairs = 0
-        triplets = 0
+        if (self.card_count(tiles) != 17): return []
 
-        for _, value in tiles.items():
+        results = {'pairs':[], 'triple':[]}
+
+        for key, value in tiles.items():
 
             #1 pair
             if value == 2:
-                pairs += 1
+                results['pairs'].append(f'{key}, {key}')
             
             #2 pairs of the same card
             elif value == 4:
-                pairs += 2
+                results['pairs'].append(f'{key}, {key}')
+                results['pairs'].append(f'{key}, {key}')
 
             #1 triplet in winning deck
             elif value == 3:
-                triplets += 1
+                results['triple'].append(f'{key}, {key}, {key}')
 
-        return True if pairs == 7 and triplets == 1 else False
+        return ['ligu hu', results] if len(results['pairs']) == 7 and len(results['triple']) == 1 else []
     
     def sixteen_bd_check(self, tiles):
+        if (self.card_count(tiles) != 17): return []
 
         #Check it has all compass and zfb
         for key in compass_dict:
             if key not in tiles:
-                return False
+                return []
             
         for key in zfb_dict:
             if key not in tiles:
-                return False
+                return []
 
         #Check at least separated by 3 for m,s,t.
         def sixteenbd_helper(dictionary):
@@ -98,41 +106,80 @@ class DeckValidator:
                     present[dictionary[key]] = True
                     temp += 1
 
-            #Check that there are at least 3 tiles
+            # Check that there are at least 3 tiles
             if temp < 3:
                 return False
-            
+
             for i in range(1, 10):
                 if present[i]:
                     if (i + 1 <= 9 and present[i + 1]) or (i + 2 <= 9 and present[i + 2]):
                         return False
-                    
-            return True
-                    
-        if not sixteenbd_helper(m_dict): return False
 
-        if not sixteenbd_helper(s_dict): return False
-        
-        if not sixteenbd_helper(t_dict): return False
-        
-        #Checks there is only 1 pair of eyes
-        temp = 0
+            return True
+
+        if not sixteenbd_helper(m_dict):return []
+        if not sixteenbd_helper(s_dict):return []
+        if not sixteenbd_helper(t_dict):return []
+
+        #Find pair of eyes
+        eyes = [key for key, value in tiles.items() if value == 2]
+        if len(eyes) != 1: return None
+
+        all_tiles = []
         for key, value in tiles.items():
-            if value == 2:
-                temp += 1
-                
-        return True if temp == 1 else False
+            all_tiles.extend([key] * value)
+
+        return ['16bd', {'eyes': eyes[0],'tiles': all_tiles}]
     
     def thirteen_waist_check(self, tiles):
+        if (self.card_count(tiles) != 17): return []
+        tiles_to_remove = []
+
         #Check it has all compass and zfb
         for key in compass_dict:
+            tiles_to_remove.append(key)
             if key not in tiles:
-                return False
+                return []
             
         for key in zfb_dict:
+            tiles_to_remove.append(key)
             if key not in tiles:
-                return False
-        return None
+                return []
+            
+        def thirteen_helper(prefix, tiles):
+            tiles_to_remove.append(f'{prefix}1')
+            tiles_to_remove.append(f'{prefix}9')
+            return f'{prefix}1' in tiles and f'{prefix}9' in tiles
+            
+        if not thirteen_helper('t', tiles): return []
+        if not thirteen_helper('s', tiles): return []
+        if not thirteen_helper('m', tiles): return []
+        
+        possible_eyes = []
+
+        for key, value in tiles.items():
+            if value >= 2:
+                temp = tiles.copy()
+                temp[key] -= 2
+                temp = clean_tiles(temp)
+                possible_eyes.append((key, temp))
+
+        for eye, remaining_tiles in possible_eyes:
+
+            for key in tiles_to_remove:
+                if key in remaining_tiles and key != eye:
+                    remaining_tiles[key] -= 1
+            remaining_tiles = clean_tiles(remaining_tiles)
+
+            complete_sets = []
+            memo = {}
+
+            if self.top_down_dfs(remaining_tiles, memo, complete_sets):
+                tiles_list = complete_sets + tiles_to_remove.copy()
+                tiles_list.append(eye)
+                return ['13waist hu', {'eye': eye, 'tiles': tiles_list}]
+
+        return []
     
     def standard_check(self, tiles):        
         #Find all possible eyes
@@ -145,17 +192,17 @@ class DeckValidator:
                 tiles = clean_tiles(tiles)
                 possible_eyes.append((key, temp))
         
-        found_valid = False
+        results = []
 
         for eye, remaining_tiles in possible_eyes:
             complete_sets = []
+            complete_sets.append(f'{eye}, {eye}')
             memo = {}
             if self.top_down_dfs(remaining_tiles, memo, complete_sets):
-                if len(complete_sets) == 5:
-                    self.possibleDecks.append(['standard', {'eye': eye, 'tiles': complete_sets}])
-                    found_valid = True
+                if len(complete_sets) == 6:
+                    results.append(['standard hu', {'eye': eye, 'tiles': complete_sets}])
 
-        return found_valid
+        return results
 
     def top_down_dfs(self, tiles, memo, complete_sets):
         if not tiles:
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from flower_counter import FlowerCounter
 from deck_validator import DeckValidator
 
 if __name__ == "__main__":
-    #fa_test = {'f1': 1, "ff1": 1, 'f2': 1, 'ff2': 1, "f3": 1, 'ff3': 1, 'f4':1, 'ff4':1, 'west' : 4, 'east': 4, 'fa': 2, 'bai': 2, 'north': 2, 'south': 3}
-    #Ligu_test = {'m1': 1, 'm5': 1, 'm9': 1, 's1': 1, 's4': 1, 's7': 1, 't1': 1, 't6': 1, 't9': 1, 'east': 2, 'south': 1, 'west': 2, 'north': 1, 'zhong': 1, 'fa': 1, 'bai':1,}
-    standard_test = {'m1': 2, 'm2':2, 'm3':2, 'm4': 2, 's1':4, "t3":4, 'east': 4}
+    fa_test = {'f1': 1, "ff1": 1, 'f2': 1, 'ff2': 1, "f3": 1, 'ff3': 1, 'f4':1, 'ff4':1, 'west' : 4, 'east': 4, 'fa': 2, 'bai': 2, 'north': 2, 'south': 3}
+    sixteenbd_test = {'m1': 1, 'm5': 1, 'm9': 1, 's1': 1, 's4': 1, 's7': 1, 't1': 1, 't6': 1, 't9': 1, 'east': 1, 'south': 1, 'west': 2, 'north': 1, 'zhong': 1, 'fa': 1, 'bai':1}
+    Thirteen_waist_test = {'m1': 1, 'm9':1, 's1': 1, 's9':1, 't1': 1, 't7':1, 't8':1, 't9':3, 'east': 1, 'south': 1, 'west': 1, 'north': 1, 'zhong': 1, 'fa': 1, 'bai':1}
+    ligu_test = {'m1': 2, 'm5': 2, 'm9': 2, 's1': 2, 's4': 2, 's7': 2, 't1': 2, 't6': 3}
+    standard_test = {'m1': 3, 'm2':2, 'm3':2, 'm4': 2, 's1':2, "t3":4, 'east': 4}
+
     winner_seat = 2
     curr_circle = 'west'
 
