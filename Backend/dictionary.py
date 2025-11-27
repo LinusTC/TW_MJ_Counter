8ff7852d02ce1ef5150082a17a4155943a760bd9
LinusTC <tongchun03@gmail.com>
2025-11-27 22:51:35 +0800

    Adding front end

---
+++ Backend/dictionary.py
@@ -0,0 +1,10 @@
+tsm_name = ['m', 't', 's']
+m_dict = {f'{tsm_name[0]}{i}': i for i in range(1, 10)}
+t_dict = {f'{tsm_name[1]}{i}': i for i in range(1, 10)}
+s_dict = {f'{tsm_name[2]}{i}': i for i in range(1, 10)}
+wind_dict = {'east', 'south', 'west', 'north'}
+zfb_dict = {'zhong', 'fa', 'bai'}
+flower_dict = {'f1' : 1, 'f2' : 2, 'f3' : 3, 'f4' : 4, 'ff1' : 1, 'ff2' : 2, 'ff3' : 3, 'ff4': 4}
+seat_dict = {1: 'east', 2: 'south', 3: 'west', 4: 'north'}
+
+mst_dict = m_dict | t_dict | s_dict
---
+++ Backend/values.py
@@ -0,0 +1,66 @@
+flower_value = 1 #done
+wind_value = 1 #done
+gong_value = 1 #done
+general_eye_value = 1 #done
+sister_value = 1 #done
+lao_shao_value = 1 #done
+flower_seat_value = 1 #done
+wind_wind_value = 1 #done
+wind_seat_value = 1 #done
+
+zfb_value = 2 #done
+flower_wind_seat_value_add_on = 1 #done
+door_clear_value = 3 #done
+door_clear_zimo_value = 5 #done
+
+sister_pong_value = 2 #done
+three_sister_pong_value = 5 #done
+
+myself_mo_value = 1 #done
+fake_solo_value = 1
+double_pong_value = 1
+real_solo_value = 2
+
+light_four_turtle_value = 3 #done
+dark_four_turtle_value = 5 #done
+ping_hu_value = 5 #done
+less_one_door_value = 5 #done
+break_waist_value = 5 #done
+noFlower_noZFB_nowind_value_add_on = 3 #done
+five_door_value = 5 #done
+three_sister_value = 5 #done
+
+ban_gao_value = 2 #done
+two_ban_gao_value = 10 #done
+three_ban_gao_value = 20 #done
+
+light_mixed_dragon_value = 5 #done
+dark_mixed_dragon_value = 10 #done
+light_same_dragon_value = 10 #done
+dark_same_dragon_value = 20 #done
+
+no_zifa_ping_hu_value_add_on = 2 #done
+small_3_wind_value = 15 #done
+big_3_wind_value = 30 #done
+dui_dui_hu_value = 30 #done
+same_house_with_fan_value = 30 #done
+small_3_zfb_value = 30 #done
+li_gu_value = 40 #done
+seven_flower_value = 20 #done
+eight_flower_value = 40 #done
+sixteenbd_value = 40 #done
+three_numbers_fan_value = 40 #done
+
+big_3_zfb_value = 60 #done
+small_4_wind_value = 60 #done
+two_numbers_fan_value = 80 #done
+three_numbers_no_fan_value = 80 #done
+all_same_house_value = 80 #done
+thirteen_waist_value = 80 #done
+big_4_wind_value = 80 #done
+one_nine_with_fan_value = 80 #done
+two_numbers_no_fan_value = 100 #done
+only_fan_value = 100 #done
+only_one_nine_value = 100 #done
+
+explode_hu_value = 50 #done
