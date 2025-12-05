4c46f78efb06469f3ce1bfdd45d6cef82bc7d5d4
LinusTC <tongchun03@gmail.com>
2025-12-05 17:43:55 +0800

    HOLY SHIT

--- backend/tile_classifier_tfl.py
+++ backend/tile_classifier_tfl.py
@@ -8,10 +8,10 @@ class TileClassifierTFL:
 		self.model_path = model_path
 		self.conf_threshold = conf
 		self.classified_decks = []
-		self._model = YOLO(self.model_path)
+		self.model = YOLO(self.model_path)
 
-	def _predict(self, stream=False):
-		return self._model.predict(
+	def predict(self, stream=False):
+		return self.model.predict(
 			source=self.image_source,
 			conf=self.conf_threshold,
 			stream=stream,
@@ -19,7 +19,7 @@ class TileClassifierTFL:
 		)
 
 	def classify_photo(self):
-		predictions = self._predict(stream=False)
+		predictions = self.predict(stream=False)
 		for result in predictions:
 			boxes = result.boxes
 			detected_tiles = {}
@@ -34,7 +34,7 @@ class TileClassifierTFL:
 			self.classified_decks.append(detected_tiles)
 
 	def classify_video(self):
-		predictions = self._predict(stream=True)
+		predictions = self.predict(stream=True)
 
 		previous_detection = None
 		stable_count = 0
